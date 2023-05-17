4877dafccffc3de70dbc8a2ddaee3ed6acdf984b
genkoskarev <gen.koskarev@gmail.com>
2023-05-18 02:21:18 +0300

    add section 'utility'

---
+++ App/Templates/components/utility.php
@@ -0,0 +1,53 @@
+<?
+$this->setCss('utility');
+?>
+
+<section class="section-utility container-lg">
+    <div class="section-utility-header">
+        <h2 class="section-utility-header-title">Утилиты</h2>
+        <h3 class="section-utility-header-description">Программы, плагины, приложения для создания игр, графики, музыки</h3>
+        <a href="/" class="section-utility-header-link">Посмотреть все</a>
+    </div>
+
+    <div class="section-utility-list">
+
+        <ul>
+            <li>
+                <a class="section-utility-item" href="/">
+                    <h4>Редактор персонажа</h4>
+                    <p class="section-utility-item-description">Редактор позволяет создавать персонажей для 2d-rpg игр с движением в четыре стороны</p>
+                </a>
+            </li>
+            <li>
+                <a class="section-utility-item" href="/">
+                    <h4>Пиксель-арт редактор</h4>
+                    <p class="section-utility-item-description">Простой редактор для рисования в стиле пиксель-арт</p>
+                </a>
+            </li>
+            <li>
+                <a class="section-utility-item" href="/">
+                    <h4>Музыкальный редактор</h4>
+                    <p class="section-utility-item-description">Программа для создания хип-хоп-композиций</p>
+                </a>
+            </li>
+            <li>
+                <a class="section-utility-item" href="/">
+                    <h4>Редактор уровней</h4>
+                    <p class="section-utility-item-description">Редактор уровней для простого платформера</p>
+                </a>
+            </li>
+            <li>
+                <a class="section-utility-item" href="/">
+                    <h4>Редактор карточек</h4>
+                    <p class="section-utility-item-description">Программа для создания карточек, коллажей с возможностью экспорта в PNG и HTML</p>
+                </a>
+            </li>
+            <li>
+                <a class="section-utility-item" href="/">
+                    <h4>Редактор статей</h4>
+                    <p class="section-utility-item-description">Редактор позволяет не только писать статьи для нашего сайта, но и экспортировать написанное в HTML-формат и в формат для форумов</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
--- App/Templates/pages/index.php
+++ App/Templates/pages/index.php
@@ -4,4 +4,6 @@ $this->Component('gallery_games');
 $this->Component('gallery');
 $this->Component('unfinished');
 $this->Component('journal');
+$this->Component('utility');
+$this->Component('gallery');
 $this->Component('articles');
