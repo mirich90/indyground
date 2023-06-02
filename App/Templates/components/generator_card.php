b8c793bab75500b35cc97dc8f67869437d8b8478
genkoskarev <gen.koskarev@gmail.com>
2023-06-02 23:23:17 +0300

    fix modal, fix card generator

--- App/Templates/components/generator_card.php
+++ App/Templates/components/generator_card.php
@@ -2,3 +2,25 @@
 $this->setCss('input');
 $this->setCss('generator_card');
 $this->setJs('generatorCard');
+?>
+
+<div id="card-generator" class="modal-wrapper">
+    <div class="modal modal-lg">
+        <div class="modal-head">
+            <p class="modal-title">Ссылки на статью</p>
+            <a class="modal-btn-close modal-trigger"></a>
+        </div>
+        <div id="card-generator-content" class="modal-content">
+            <?= $this->ui('input', [
+                'id' => 'generator-title',
+                'label' => 'Заголовок (необязательно)',
+                'placeholder' => "Введите название карточки",
+            ]); ?>
+            <?= $this->ui('input', [
+                'id' => 'generator-text',
+                'label' => 'Текст',
+                'placeholder' => "Введите текст карточки",
+            ]); ?>
+        </div>
+    </div>
+</div>
--- public/minjs/generatorCard.js
+++ public/minjs/generatorCard.js
@@ -4,32 +4,12 @@ $click(".create-card", (e) => {
 
 class CardGenerator {
   constructor() {
-    // this.generatorDiv = $create();
-    this.modal = $(".modal-wrapper");
-    this.modalContent = $(".modal-content");
+    this.modal = $("#card-generator");
+    this.modalContent = $("#card-generator-content");
 
-    this.setModalTitle();
-    this.setModalContent();
     this.openModal();
-    // modal.append(qrcodeDiv);
-  }
-
-  setModalTitle() {
-    $(".modal-title").innerText = "Создать карточку";
-  }
-  setModalContent() {
-    $(".modal-content").innerHTML = `
-    <div class="input__wrapper">
-      <label class="label">Заголовок (необязательно)</label>
-      <input id="generator-title" class="input" type="text" placeholder="Введите название карточки" value="">
-    </div>
-    <div class="input__wrapper">
-      <label class="label">Текст</label>
-      <input id="generator-text" class="input" type="text" placeholder="Введите текст карточки" value="">
-    </div>
-    `;
   }
   openModal() {
-    $classAdd($(".modal-wrapper"), "open");
+    $classAdd(this.modal, "open");
   }
 }
