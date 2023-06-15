a238b04b845e119189ab06e10e172d40cb659f5e
genkoskarev <gen.koskarev@gmail.com>
2023-06-15 23:57:57 +0300

    fix modal

--- App/Templates/components/modal.php
+++ App/Templates/components/modal.php
@@ -3,13 +3,13 @@ $this->setJs('modal');
 $this->setCss('modal');
 ?>
 
-<div class="modal-wrapper">
-    <div class="modal">
-        <div class="modal-head">
-            <p class="modal-title"></p>
-            <a class="modal-btn-close modal-trigger"></a>
+<div id="modal-wrapper" class="modal-wrapper">
+    <div id="modal" class="modal">
+        <div id="modal-head" class="modal-head">
+            <p id="modal-title" class="modal-title"></p>
+            <a id="modal-btn-close" class="modal-btn-close modal-trigger"></a>
         </div>
-        <div class="modal-content">
+        <div id="modal-content" class="modal-content">
         </div>
     </div>
 </div>
