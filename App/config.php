5cf3c2f631e1a517cebadc95113c38de9dba9584
genkoskarev <gen.koskarev@gmail.com>
2024-01-24 18:15:10 +0300

    fix

--- App/config.php
+++ App/config.php
@@ -4,7 +4,7 @@ return [
   'db' => [
     'servername' => "localhost",
     'username' => "test2",
-    'password' => "***",
+    'password' => "Pkjq15041990",
     'database' => "test2"
   ],
   'is_local' => true
